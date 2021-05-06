607c012d90fe46b8e2a96f9d06bf493f3ae8d3d9
devyaneedalmia <77791378+devyaneedalmia@users.noreply.github.com>
2021-05-06 19:59:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,25 +5,27 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
+var passedFinish;
 var obstacles;
-var s;
-var passFinish;
-
+var s,i;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-var i, track, car1_img, car2_img, car3_img, car4_img;
+var track, car1_img, car2_img, car3_img, car4_img;
+
 
 function preload(){
-  f2 = loadImage("images/f1.png");
-s=loadSound("sounds/sliding.mp3")
-
+    s=loadSound("sound/sliding.mp3")
+f2 = loadImage("images/f1.png");
   track = loadImage("images/track.jpg");
   car1_img = loadImage("images/car1.png");
   car2_img = loadImage("images/car2.png");
   car3_img = loadImage("images/car3.png");
   car4_img = loadImage("images/car4.png");
   ground = loadImage("images/ground.png");
+  bronze_img = loadImage("images/bronze.png");
+  silver_img = loadImage("images/silver.png");
+  gold_img = loadImage("images/gold.png");
 }
 
 function setup(){
@@ -31,7 +33,7 @@ function setup(){
   database = firebase.database();
   gameState = 0;
   distance = 0;
- // finishedPlayers = 0;
+  finishedPlayers = 0;
   yVel = 0;
   xVel = 0;
   obstacles=createGroup();
@@ -40,7 +42,7 @@ function setup(){
   game = new Game();
   game.getState();
   game.start();
-  for(i=0;i<5;i++)
+    for(i=0;i<5;i++)
   {
     w=random(200,950);
     h=random(-height*4,height-300);
@@ -52,28 +54,37 @@ function setup(){
 }
 
 
-function draw()
-{
+function draw(){
    //start the game
    background(200, 200, 255);
 
-   if(playerCount === 4 && finishedPlayers === 0)
-   {
-     game.update(1);
-   }
+   //start the game
  
-   //start the game for real
-   if (gameState === 1) {
-     game.play();
-   }
-   if (gameState === 2) {
-     console.log("End");
-   }
+  //start the game
+  if (playerCount === 4 && finishedPlayers === 0) {
+    game.update(1);
+  }
 
-   if (finishedPlayers === 4)
-   {
-     game.update(2);
-   }
+  //start the game for real
+  if (gameState === 1) {
+    game.play();
+  }
+
+  //end the game
+  if (finishedPlayers === 4) {
+    game.update(2);
+    //gameState = 2;
+  }
+
+  //display ranking
+  if (gameState === 2 && finishedPlayers === 4) {
+    game.displayRanks();
+  }
 }
- 
-  
+function keyPressed() {
+  if (keyCode === 13 && gameState !== 1 && passedFinish === false) {
+    form.enter();
+    console.log("hai");
+    passedFinish = true;
+  }
+}
